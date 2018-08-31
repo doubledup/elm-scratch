c260ff500cf71e32fca2bd2c1a3a804e42fc2aca
David Dunn <26876072+doubledup@users.noreply.github.com>
2018-08-31 19:33:09 +0200

    Add random number exercise

---
+++ src/random_custom.elm
@@ -0,0 +1,84 @@
+module Main exposing (..)
+
+import Browser
+import Html exposing (..)
+import Html.Events exposing (..)
+import Html.Attributes exposing (..)
+import Random
+
+
+-- MAIN
+
+
+main =
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { dieFace : Int
+    }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Model 1
+    , Cmd.none
+    )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Roll
+    | NewFace Int
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Roll ->
+            ( model
+            , Random.generate NewFace (Random.int 1 6)
+            )
+
+        NewFace newFace ->
+            ( Model newFace
+            , Cmd.none
+            )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h1 [] [ text (String.fromInt model.dieFace) ]
+        , button [ onClick Roll ] [ text "Roll" ]
+        , div []
+            [ img
+                [ src ("https://wpclipart.com/recreation/games/dice/die_face_" ++ (String.fromInt model.dieFace) ++ ".png"), height 100, width 130 ]
+                []
+            ]
+        ]
