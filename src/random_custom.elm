c24751a7113ccf1b697a7784a98a16f6e66c01c3
David Dunn <26876072+doubledup@users.noreply.github.com>
2018-09-01 18:45:40 +0200

    Add weighted die with simple animation

--- src/random_custom.elm
+++ src/random_custom.elm
@@ -5,6 +5,8 @@ import Html exposing (..)
 import Html.Events exposing (..)
 import Html.Attributes exposing (..)
 import Random
+import Task
+import Process
 
 
 -- MAIN
@@ -25,12 +27,17 @@ main =
 
 type alias Model =
     { dieFace : Int
+    , weightedDieFace : Int
+    , rollCounter : Int
     }
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( Model 1
+    ( { dieFace = 1
+      , weightedDieFace = 1
+      , rollCounter = 0
+      }
     , Cmd.none
     )
 
@@ -41,21 +48,49 @@ init _ =
 
 type Msg
     = Roll
+    | StartRoll
     | NewFace Int
+    | WeightedFace Int
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Roll ->
-            ( model
-            , Random.generate NewFace (Random.int 1 6)
-            )
+            if model.rollCounter > 0 then
+                ( { model | rollCounter = model.rollCounter - 1 }
+                , Cmd.batch [ (Task.perform (\_ -> Roll) (Process.sleep 100.0)), rollDice ]
+                )
+            else
+                ( model, Cmd.none )
+
+        StartRoll ->
+            ( { model | rollCounter = 10 }, Task.perform (\_ -> Roll) (Process.sleep 1.0) )
 
         NewFace newFace ->
-            ( Model newFace
+            ( { model | dieFace = newFace }
             , Cmd.none
             )
+
+        WeightedFace newFace ->
+            ( { model | weightedDieFace = newFace }
+            , Cmd.none
+            )
+
+
+rollDice =
+    Cmd.batch
+        [ Random.generate NewFace (Random.int 1 6)
+        , Random.generate WeightedFace
+            (Random.weighted ( 1, 1 )
+                [ ( 1, 2 )
+                , ( 2, 3 )
+                , ( 3, 4 )
+                , ( 5, 5 )
+                , ( 8, 6 )
+                ]
+            )
+        ]
 
 
 
@@ -75,10 +110,17 @@ view : Model -> Html Msg
 view model =
     div []
         [ h1 [] [ text (String.fromInt model.dieFace) ]
-        , button [ onClick Roll ] [ text "Roll" ]
+        , button [ onClick StartRoll ] [ text "Roll" ]
         , div []
-            [ img
+            [ h3 [] [ text "Fair die" ]
+            , img
                 [ src ("https://wpclipart.com/recreation/games/dice/die_face_" ++ (String.fromInt model.dieFace) ++ ".png"), height 100, width 130 ]
+                []
+            ]
+        , div []
+            [ h3 [] [ text "Weighted die" ]
+            , img
+                [ src ("https://wpclipart.com/recreation/games/dice/die_face_" ++ (String.fromInt model.weightedDieFace) ++ ".png"), height 100, width 130 ]
                 []
             ]
         ]
