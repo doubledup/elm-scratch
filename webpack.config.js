93d790ed5836d9cb597d54303b0652f90e9b4a1b
David Dunn <26876072+doubledup@users.noreply.github.com>
2018-11-09 11:54:38 +0200

    Add uglifyjs to webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
   entry: './public/index.js',
@@ -38,5 +39,20 @@ module.exports = {
     ],
 
     noParse: /\.elm$/
+  },
+
+  optimization: {
+    minimizer: [new UglifyJsPlugin({
+      uglifyOptions: {
+        compress: {
+          pure_funcs: 'F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9',
+          pure_getters: true,
+          keep_fargs: false,
+          unsafe_comps: true,
+          unsafe: true
+        },
+        mangle: true
+      }
+    })]
   }
 }
